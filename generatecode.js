bb273be10c807b4184272debdc50abfe7d647fcd
johns2s <2392759-johns2s@users.noreply.gitlab.com>
2020-08-27 21:39:57 -0400

    * room provisioning/joining

--- generatecode.js
+++ generatecode.js
@@ -1,14 +1,5 @@
-
-
-document.addEventListener("DOMContentLoaded", function() {
-
-  window.addEventListener("load", function() {
-    function rand(min, max) {
-      let ranNum = Math.random() * (max - min) + min;
-      return Math.floor(ranNum);
-      console.log(1)
-    }
-    document.getElementById('randomNum').value = rand(10, 20)
-
-  });
-  });
+function randomCode(min, max) {
+  let ranNum = Math.random() * (max - min) + min;
+  return Math.floor(ranNum);
+}
+document.getElementById('randomNum').textContent = randomCode(100,999);
--- script.js
+++ script.js
@@ -5,16 +5,89 @@ function changeMainViews(view) {
   document.querySelector(`.main .main__section.main__${view}`).classList.remove("main__section--hidden");
 }
 
-document.querySelector(".button[data-action='create']").addEventListener("click", () => {
-  changeMainViews("create")
-});
+document.querySelectorAll(".button[data-action='create']").forEach((el) => {
+  el.addEventListener("click", () => {
+    changeMainViews("create");
+  });
+})
 
-document.querySelector(".button[data-action='join']").addEventListener("click", () => {
-  changeMainViews("join")
+document.querySelectorAll(".button[data-action='join']").forEach((el) => {
+  el.addEventListener("click", () => {
+    changeMainViews("join");
+  });
 });
 
 document.querySelectorAll(".button[data-action='goToSplash']").forEach((el) => {
   el.addEventListener("click", () => {
-    changeMainViews("splash")
+    changeMainViews("splash");
   });
-})
+});
+
+document.querySelectorAll(".button[data-action='submitCreateRoom']").forEach((el) => {
+  el.addEventListener("click", () => {
+    createRoom();
+  });
+});
+
+document.querySelectorAll(".button[data-action='submitJoinRoom']").forEach((el) => {
+  el.addEventListener("click", () => {
+    joinRoom();
+  });
+});
+
+function randomCode(min, max) {
+  let ranNum = Math.random() * (max - min) + min;
+  return String(Math.floor(ranNum));
+}
+
+let firebaseDB;
+
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    firebaseDB = firebase.firestore();
+    user = user.uid;
+  }
+  else {
+    firebase.auth().signInAnonymously().catch(function(error) {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log("FUCK!!");
+    });
+  }
+});
+
+async function createRoom() {
+  const nickname = document.querySelector("#create__enterNickname").value;
+  let roomCode = randomCode(100, 999);
+  try {
+    await firebaseDB.collection("rooms").doc(roomCode).collection('messages').doc().set({
+      timestamp: new Date(),
+      nickname: nickname,
+      content: "Room code: " + roomCode
+    });
+  }
+  catch(error) {
+    alert(`Fuck! Error: ${error}`);
+  }
+  // move them to chat page, give chat page nickname
+  window.location =`chat.html?room=${roomCode}&nickname=${nickname}`;
+}
+
+async function joinRoom() {
+  const nickname = document.querySelector("#join__enterNickname").value;
+  const roomCode = document.querySelector("#join__enterCode").value;
+  let room;
+  try {
+    room = await firebaseDB.collection("rooms").doc(roomCode).get()
+  }
+  catch(error) {
+    alert(`Fuck! Error: ${error}`);
+  }
+  if (!room.empty) {
+    window.location =`chat.html?room=${roomCode}&nickname=${nickname}`;
+  }
+  else {
+    alert("You moron, that room doesn't exist");
+  }
+
+}
