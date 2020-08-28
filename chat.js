0e7e1a25434133706e2acce1b397979bff9b21b4
johns2s <2392759-johns2s@users.noreply.gitlab.com>
2020-08-27 22:14:03 -0400

    Start chat page

---
+++ chat.js
@@ -0,0 +1,46 @@
+let firebaseDB;
+const urlParams = new URLSearchParams(window.location.search);
+
+// return user if URL not right
+if (!(urlParams.has("nickname") && urlParams.has("room"))) {
+  window.location = "index.html";
+}
+
+// get URL params
+const roomCode = urlParams.get("room");
+const nickname = urlParams.get("nickname");
+
+// return user if not authenticated
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    firebaseDB = firebase.firestore();
+    provisionChat();
+  }
+  else {
+    window.location = "index.html";
+  }
+});
+
+async function provisionChat() {
+  // check for room existence
+  let room;
+  try {
+    room = await firebaseDB.collection("rooms").doc(roomCode).get()
+  }
+  catch(error) {
+    alert(`Fuck! Error: ${error}`);
+  }
+  if (room.empty) {
+    window.location = "index.html";
+  }
+
+  // get room chats/etc
+}
+
+
+// Test
+alert(roomCode);
+
+
+// Get room chats-stuffs, etc
+// TODO
